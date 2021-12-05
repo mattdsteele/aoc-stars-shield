23ff7efa710f5a1539bd949242adae6680d10577
mattdsteele <orphum@gmail.com>
2021-12-04 22:04:07 -0600

    Add ignore, update local deploy fn

--- deploy-fn.sh
+++ deploy-fn.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy Shields --runtime go113 --env-vars-file ./.env.local.yaml --trigger-http
+gcloud functions deploy Shields --runtime go116 --env-vars-file ./.env.local.yaml --trigger-http
